b28f59753ccf3d834e87552685d982febc3381d8
Steven Silvester <steven.silvester@ieee.org>
2017-02-28 04:45:43 -0600

    wip ajax tests

--- test/ajax.spec.ts
+++ test/ajax.spec.ts
@@ -41,10 +41,50 @@ describe('@jupyterlab/coreutils', () => {
     describe('#request()', () => {
 
       it('should make an asynchronous xml request', () => {
-        let promise = AJAX.request('foo', { requestConstructor: xhr as any });
+        let promise = AJAX.request('foo', { xhr: xhr as any });
         let request = requests[0];
         request.respond(200, {}, 'foo');
         return expect(promise).to.be.fulfilled;
+      });
+
+      it('should populate defaults', () => {
+        AJAX.request('foo', { xhr: xhr as any });
+        let request = requests[0];
+        expect(request.method).to.equal('GET');
+        expect(request.password).to.equal('');
+        expect(request.requestBody).to.equal(null);
+        expect(request.requestHeaders['Content-Type']).to.equal('application/json');
+        expect(request.url).to.contain('foo?');
+        expect(request.username).to.equal('');
+        expect(request.withCredentials).to.equal(undefined);
+      });
+
+      it('should allow overrides', () => {
+        let settings = {
+          method: 'POST',
+          dataType: 'application/json',
+          contentType: 'application/json',
+          data: { 'foo': 1, 'bar': 'baz' },
+          cache: false,
+          timeout: 10,
+          requestHeaders: { 'foo': 'bar' },
+          withCredentials: true,
+          user: 'snuffy',
+          password: 'password',
+          token: 'secret',
+          xhr: xhr as any
+        };
+        AJAX.request('foo', settings);
+        let request = requests[0];
+        expect(request.method).to.equal('POST');
+        expect(request.password).to.equal('password');
+        expect(request.requestBody).to.be.deep.equal(settings.data);
+        expect(request.requestHeaders['Content-Type']).to.contain('application/json');
+        expect(request.requestHeaders['Authorization']).to.equal('token secret');
+        expect(request.requestHeaders['foo']).to.equal('bar');
+        expect(request.url).to.not.contain('?');
+        expect(request.username).to.equal('snuffy');
+        expect(request.withCredentials).to.equal(true);
       });
 
     });
